045bf231ee8e932e41f9d1989aafec787b865cdf
Kevin A. Carson <112520273+kevinCarson@users.noreply.github.com>
2026-01-13 11:16:04 -0700

    Update rem_fourcyles_deprecatedV100.R

--- R/rem_fourcyles_deprecatedV100.R
+++ R/rem_fourcyles_deprecatedV100.R
@@ -43,6 +43,7 @@
 #'
 #' @description
 #' `r lifecycle::badge("deprecated")`
+#'
 #' `computeFourCycles()` has been deprecated in the newest update to the `dream` package. Please use the `remstats_fourcycles()`and see the `NEWS.md`.
 #'
 #'
