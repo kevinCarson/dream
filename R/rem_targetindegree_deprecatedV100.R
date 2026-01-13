ac4c8f24f63b9d51c89a9ed54956c7cebd75c4fd
Kevin A. Carson <112520273+kevinCarson@users.noreply.github.com>
2026-01-13 11:19:19 -0700

    Update rem_targetindegree_deprecatedV100.R

--- R/rem_targetindegree_deprecatedV100.R
+++ R/rem_targetindegree_deprecatedV100.R
@@ -35,6 +35,7 @@
 
 #' @description
 #' `r lifecycle::badge("deprecated")`
+#'
 #' `computeReceiverIndegree()` has been deprecated in the newest update to the `dream` package. Please use the `remstats_degree()`and see the `NEWS.md`.
 #'
 #'
